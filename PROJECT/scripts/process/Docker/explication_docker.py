cb8e7bdddbb14fb20c0ac8919d2fdc2f8354bf85
Yuanlong <pyldaniel@yeah.net>
2024-05-18 18:56:26 +0200

    add explication pour docker

---
+++ PROJECT/scripts/process/Docker/explication_docker.py
@@ -0,0 +1,9 @@
+"""
+explications d'installation de Docker : il semble qu'on ne peut pas télécharger la version récente de Docker pour la version de mon mac, j'ai décidé donc de suivre les commandes suivantes pour attenidre les objectifs : 
+Installation :  
+1. brew install qemu
+2. brew install docker
+3. brew install colima
+4. colima start
+5. docker --version
+""" 
